fad7634d72054b28eeca75ce9e6459efb9b9cd5c
Simeon Voelkel <simeon_voelkel@arcor.de>
2008-03-20 09:36:40 +0100

    Adding h/H keyboard shotcuts to toggle track->play
    h and H work now just as TAB and SHIFT+TAB on track->record
    This gives Cinelerra a new shortcut on the timeline.

--- cinelerra/mwindowgui.C
+++ cinelerra/mwindowgui.C
@@ -469,6 +469,8 @@ int MWindowGUI::keypress_event()
 
 	if(!result)
 	{
+				int cursor_x = canvas->get_relative_cursor_x();
+				int cursor_y = canvas->get_relative_cursor_y();
 		switch(get_keypress())
 		{
 			case 'e':
@@ -584,8 +586,6 @@ int MWindowGUI::keypress_event()
 
 			case TAB:
 			case LEFTTAB:
-				int cursor_x = canvas->get_relative_cursor_x();
-				int cursor_y = canvas->get_relative_cursor_y();
 
 				if(get_keypress() == TAB)
 				{
@@ -675,7 +675,111 @@ int MWindowGUI::keypress_event()
 
 				result = 1;
 				break;
+	
+/*
+ * Mulitcamhack: let h toggle the visibility of tracks just as TAB toggles 
+ * the armed / unarmed status
+ * */
+		case 'h':
+		case 'H':
+
+				if(get_keypress() == 'h')
+				{
+// Switch the play button
+					for(Track *track = mwindow->edl->tracks->first; track; track = track->next)
+					{
+						int64_t track_x, track_y, track_w, track_h;
+						canvas->track_dimensions(track, track_x, track_y, track_w, track_h);
+
+						if(cursor_y >= track_y && 
+							cursor_y < track_y + track_h)
+						{
+							if (track->play)
+								track->play = 0;
+							else
+								track->play = 1;
+
+							result = 1; 
+							break;
+						}
+					}
+				} 
+				else 
+				{
+					Track *this_track = 0;
+					for(Track *track = mwindow->edl->tracks->first; track; track = track->next)
+					{
+						int64_t track_x, track_y, track_w, track_h;
+						canvas->track_dimensions(track, track_x, track_y, track_w, track_h);
+
+						if(cursor_y >= track_y && 
+							cursor_y < track_y + track_h)
+						{
+							// This is our track
+							this_track = track;
+							break;
+						}
+					}
+
+					int total_selected = mwindow->edl->tracks->total_of(Tracks::PLAY);
+
+	// nothing previously selected
+					if(total_selected == 0)
+					{
+						mwindow->edl->tracks->select_all(Tracks::PLAY,
+							1);
+					}
+					else
+					if(total_selected == 1)
+					{
+	// this patch was previously the only one on
+						if(this_track && this_track->play)
+						{
+							mwindow->edl->tracks->select_all(Tracks::PLAY,
+								1);
+						}
+	// another patch was previously the only one on
+						else
+						{
+							mwindow->edl->tracks->select_all(Tracks::PLAY,
+								0);
+							if (this_track) 
+								this_track->play = 1;
+
+						}
+					}
+					else
+					if(total_selected > 1)
+					{
+						mwindow->edl->tracks->select_all(Tracks::PLAY,
+							0);
+						if (this_track) 
+							this_track->play = 1;
+					}
+
+				}
+
+				update (0,
+						1,
+						0,
+						0,
+						1,
+						0,
+						1);
+				unlock_window();
+				mwindow->cwindow->update(0, 1, 1);
+				lock_window("TrackCanvas::keypress_event 3");
+
+				result = 1;
+				break;
+
+/*
+ *
+ * End of Mulicamhack
+ *
+ * */
 		}
+
 
 // since things under cursor have changed...
 		if(result) 
